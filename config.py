0e00b49b6c7f8bf1ac76414e438bd276d61dc4ca
Yogesh Shukla <shuklayogesh5635@gmail.com>
2026-08-20 12:26:38 +0530

    Use environment variable for secret key

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = "yogesh_shukla_5635"
+    SECRET_KEY = os.getenv("SECRET_KEY", "yogesh_shukla_5635")
 
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
